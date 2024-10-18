cc52851240ddfdca8ecebd37ca0e5d75e2fa3947
jyks957 <jyks957@naver.com>
2024-10-18 11:48:26 +0900

    ch02 끝. react-router를 통해 화면이동, 쿼리스트링, useNavigate, useParams, useSearchParams, createParams .. 등 학습

---
+++ mall/src/pages/todo/ReadPage.js
@@ -0,0 +1,45 @@
+
+import { useCallback } from 'react';
+import { useParams, useNavigate, createSearchParams, useSearchParams } from 'react-router-dom';
+
+const ReadPage = () => {
+
+  const {tno} = useParams()
+
+  const navigate = useNavigate()
+
+  const [queryParams] = useSearchParams()
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+
+  const queryStr = createSearchParams({page,size}).toString()
+
+  const moveToModify = useCallback((tno) => {
+    navigate({
+      pathname:`/todo/modify/${tno}`,
+      search: queryStr
+    })
+  }, [tno, page, size])
+
+  const moveToList = useCallback(() => {
+    navigate({
+      pathname:`/todo/list`,
+      search: queryStr
+    }, [page, size])
+  })
+
+  return(
+    <div className="text-3xl font-extrabold">
+      Todo Read Page Component {tno}
+
+      <div>
+        <button onClick={() => moveToModify(tno)}>Test Modify</button><br />
+        <button onClick={() => moveToList()}>Test List</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default ReadPage;
