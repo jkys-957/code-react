8c8a9b49cc020726d996918cb687569e45714157
jkys957 <jkys957@naver.com>
2024-10-25 17:49:45 +0900

    스프링부트 API 통신, 리액트 페이징 처리 첫 작업

--- mall/src/pages/todo/ReadPage.js
+++ mall/src/pages/todo/ReadPage.js
@@ -1,43 +1,38 @@
-
-import { useCallback } from 'react';
-import { useParams, useNavigate, createSearchParams, useSearchParams } from 'react-router-dom';
+//import { useCallback } from 'react'
+import { useParams } from 'react-router-dom'
+import ReadComponent from '../../components/todo/ReadComponent'
 
 const ReadPage = () => {
 
   const {tno} = useParams()
 
-  const navigate = useNavigate()
+//  const navigate = useNavigate()
+//  const [queryParams] = useSearchParams()
+//  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+//  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+//  const queryStr = createSearchParams({page,size}).toString()
 
-  const [queryParams] = useSearchParams()
+  // const moveToModify = useCallback((tno) => {
+  //   navigate({
+  //     pathname:`/todo/modify/${tno}`,
+  //     search: queryStr
+  //   })
+  // }, [tno, page, size])
 
-  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
-  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
-
-  const queryStr = createSearchParams({page,size}).toString()
-
-  const moveToModify = useCallback((tno) => {
-    navigate({
-      pathname:`/todo/modify/${tno}`,
-      search: queryStr
-    })
-  }, [tno, page, size])
-
-  const moveToList = useCallback(() => {
-    navigate({
-      pathname:`/todo/list`,
-      search: queryStr
-    }, [page, size])
-  })
+  // const moveToList = useCallback(() => {
+  //   navigate({
+  //     pathname:`/todo/list`,
+  //     search: queryStr
+  //   }, [page, size])
+  // })
 
   return(
-    <div className="text-3xl font-extrabold">
-      Todo Read Page Component {tno}
-
-      <div>
-        <button onClick={() => moveToModify(tno)}>Test Modify</button><br />
-        <button onClick={() => moveToList()}>Test List</button>
+    <div className="w-full mt-6 font-extrabold bg-white">
+      <div className="text-2xl">
+        Todo Read Page Componet {tno}
       </div>
-      
+
+      <ReadComponent tno={tno}></ReadComponent>
     </div>
   );
 }
