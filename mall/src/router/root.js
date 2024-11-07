2d3b6b834bf57effaad04f72526d5e91663621be
jkys957 <jkys957@naver.com>
2024-11-07 17:48:39 +0900

    ch06 리액트와 상품 API 서버 연동 시작

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -1,14 +1,18 @@
 import { Suspense, lazy } from "react";
 import todoRouter from '../router/todoRouter';
+import productsRouter from '../router/productsRouter';
 
 const { createBrowserRouter } = require("react-router-dom");
 
 const Loading = <div>Loading...</div>
+
 const Main = lazy(() => import("../pages/MainPage"))
 
 const About = lazy(() => import("../pages/AboutPage"))
 
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+
+const ProductsIndex = lazy(() => import("../pages/products/IndexPage"))
 
 const root = createBrowserRouter([
   {
@@ -17,12 +21,17 @@ const root = createBrowserRouter([
   },
   {
     path: "about",
-    element: <Suspense fallback={About}><About /></Suspense>
+    element: <Suspense fallback={Loading}><About /></Suspense>
   },
   {
     path: "todo",
-    element: <Suspense fallback={TodoIndex}><TodoIndex /></Suspense>,
+    element: <Suspense fallback={Loading}><TodoIndex /></Suspense>,
     children: todoRouter()
+  },
+  {
+    path: "products",
+    element: <Suspense fallback={Loading}><ProductsIndex /></Suspense>,
+    children: productsRouter()
   }
 ])
 
